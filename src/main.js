5186c8e82581cad09f9287b38cff41d2ed549310
JulioLaptop <jcezarmaricaua@gmail.com>
2026-06-12 19:16:43 -0400

    Export consts

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 const p = require("./service/product.js")
+const config = require("./service/config.js")
 
 async function main() {
     console.log("Carrinho")
-    p.getFullName(183, "Headset")
+
+    // p.getFullName(183, "Headset")
+    // p.getProductPrice(183, 141,53)
+
+    console.log(config.devArea.version)
+    console.log(config.client)
+
 }
 
 main()
--- src/service/product.js
+++ src/service/product.js
@@ -3,6 +3,15 @@ async function getFullName(codeId, productName) {
     console.log("Produtos: " + codeId + "--" + productName);
 }
 
+async function getProductPrice(codeId, productPrice) {
+    console.log("Preço: " + codeId + "--" + productPrice);
+}
+
+async function getProductPrice(cupomDesconto) {
+    console.log("Cupom: " + codeId + "--" + productPrice);
+}
+
 module.exports = {
     getFullName,
+    getProductPrice,
 };
