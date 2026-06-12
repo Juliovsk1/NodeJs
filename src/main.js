29505dd030e2f913873c1b1ace707a118c477586
JulioLaptop <jcezarmaricaua@gmail.com>
2026-06-12 19:48:00 -0400

    Export default

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 const product = require("./service/product.js")
 const config = require("./service/config.js")
+const database = require("./service/database.js")
 
 async function main() {
     console.log("Carrinho")
@@ -7,9 +8,13 @@ async function main() {
     product.getFullName(183, "Headset")
     product.getProductPrice(20, 183,53)
 
-    console.log(config.devArea.version)
-    console.log(config.client)
+    console.log(config.devArea.version);
+    console.log(config.client);
 
+    database.connectDataBase("market");
+    console.log("Sucess!");
+    database.desconnectDataBase("amrket");
+    console.log("Bye bye!");
 }
 
 main()
---
+++ src/service/database.js
@@ -0,0 +1,9 @@
+// Export default
+
+exports.connectDataBase = (dbName) => {
+    console.log("Connection with " + dbName);
+};
+
+exports.desconnectDataBase = (dbName) => {
+    console.log("Desconnection with " + dbName);
+}
