66cb8f2e42ecbb985a81c205f20eb6c6dcc7b4a0
JulioLaptop <jcezarmaricaua@gmail.com>
2026-06-12 19:59:19 -0400

    Desestruturação Destructuring

--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
-const product = require("./service/product.js")
+// export by function
+const {getFullName} = require("./service/product.js")
+// export standard
 const config = require("./service/config.js")
 const database = require("./service/database.js")
 
 async function main() {
     console.log("Carrinho")
 
-    product.getFullName(183, "Headset")
-    product.getProductPrice(20, 183,53)
+    // product.getFullName(183, "Headset")
+    // product.getProductPrice(20, 183,53)
+    getFullName(123, "Brain")
 
     console.log(config.devArea.version);
     console.log(config.client);
