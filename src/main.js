301ff9f3968af0a7f5a878779ff5ea6a3fb00dcf
JulioLaptop <jcezarmaricaua@gmail.com>
2026-06-12 19:36:59 -0400

    Hidden members

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
-const p = require("./service/product.js")
+const product = require("./service/product.js")
 const config = require("./service/config.js")
 
 async function main() {
     console.log("Carrinho")
 
-    // p.getFullName(183, "Headset")
-    // p.getProductPrice(183, 141,53)
+    product.getFullName(183, "Headset")
+    product.getProductPrice(20, 183,53)
 
     console.log(config.devArea.version)
     console.log(config.client)
--- src/service/product.js
+++ src/service/product.js
@@ -1,15 +1,27 @@
 // Módulo para todas as funções de produto
 async function getFullName(codeId, productName) {
     console.log("Produtos: " + codeId + "--" + productName);
-}
+};
 
 async function getProductPrice(codeId, productPrice) {
     console.log("Preço: " + codeId + "--" + productPrice);
-}
+    await doBreak();
+};
 
-async function getProductPrice(cupomDesconto) {
-    console.log("Cupom: " + codeId + "--" + productPrice);
-}
+// hidden function
+async function doBreak(){
+    console.log("\n");
+};
+
+// hidden object
+const apiLocal = {
+    url: "www.google.com"
+};
+
+async function getProductPrice(cupomDesconto, codeId, productPrice) {
+    desconto = productPrice - cupomDesconto;
+    console.log("Cupom: " + codeId + " -- Preço: " + desconto);
+};
 
 module.exports = {
     getFullName,
